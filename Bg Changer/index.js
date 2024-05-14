e8bb149bf10b9b34e759e9f1b65092559692a02f
anish2210 <anishjaiswal1220@gmail.com>
2024-05-14 19:11:36 +0530

    update

---
+++ Bg Changer/index.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".button")
+
+buttons.forEach( function(button){
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === "grey") {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === "white") {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === "blue") {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === "yellow") {
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
